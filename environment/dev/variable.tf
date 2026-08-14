a59009a6ab5d109c60c353b91c138af1b518e463
Ayush2986 <ayushthehaven@gmail.com>
2026-08-14 23:49:27 +0530

    Tflint resolve001

--- environment/dev/variable.tf
+++ environment/dev/variable.tf
@@ -1,21 +1,24 @@
 variable "resource_group" {
-    type = map(object{
-        name = string
-        location = string
-    })
+  type = map(object({
+    name     = string
+    location = string
+  }))
 }
+
 variable "virtual_network" {
-   type = map(object{
-        name = string
-        location = string
-        resource_group_name = string
-        address_space = list(string)    })
+  type = map(object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    address_space       = list(string)
+  }))
 }
+
 variable "subnets" {
-   type = map(object{
-        name = string
-        resource_group_name = string
-        virtual_network_name = string
-        address_prefixes = list(string)
-    })
+  type = map(object({
+    name                 = string
+    resource_group_name  = string
+    virtual_network_name = string
+    address_prefixes     = list(string)
+  }))
 }
