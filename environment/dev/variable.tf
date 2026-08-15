2ffed700f6e4e9a1e073dec506a6d1cae1a31d19
Ayush2986 <ayushthehaven@gmail.com>
2026-08-16 00:51:41 +0530

    15 aug change021

--- environment/dev/variable.tf
+++ environment/dev/variable.tf
@@ -35,7 +35,7 @@ variable "nsg" {
 
 variable "NSG_association" {
   type = map(object({
-    subnet_id                 = string
-    network_security_group_id = string    
+    subnet_key = string
+    nsg_key    = string
   }))
 }
