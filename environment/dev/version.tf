c3f4b7cc652bb788e7c17a74b0eefcc44618d46b
Ayush2986 <ayushthehaven@gmail.com>
2026-08-18 00:55:06 +0530

    add storage account

--- environment/dev/version.tf
+++ environment/dev/version.tf
@@ -6,4 +6,10 @@ terraform {
       version = "5.0.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-ayush"
+    storage_account_name = "ayushstorage12"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
